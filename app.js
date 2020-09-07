79d3411f9a80cd4f5881f20186208a6428eb55b7
nhattan9 <nguyenquang954@gmail.com>
2020-09-07 15:58:08 +0700

    init

---
+++ app.js
@@ -0,0 +1,95 @@
+//overlay
+
+TweenMax.to('.first',1.5,{
+    delay:0.1,
+    top:'-100%',
+    ease: Expo.easeInOut,
+})
+TweenMax.to('.second',1.5,{
+    delay:0.3,
+    top:'-100%',
+    ease: Expo.easeInOut,
+})
+TweenMax.to('.third',1.5,{
+    delay:0.6,
+    top:'-100%',
+    ease: Expo.easeInOut,
+})
+
+
+//Header Animations 
+
+TweenMax.from('.logo img',1.5,{
+    delay:0.6,
+    y:'-100%',
+    ease: Expo.easeInOut,
+})
+TweenMax.from('nav ul li a',1.5,{
+    delay:1.4,
+    y:'-100',
+    ease: Expo.easeInOut,
+})
+
+//Product aniamtions
+TweenMax.from('.product h1  .hide',1.5,{
+    delay:2,
+    y:'100%',
+    ease: Expo.easeInOut,
+})
+
+TweenMax.from('.product h3  .hide',1.5,{
+    delay:2.4,
+    y:'100%',
+    ease: Expo.easeInOut,
+})
+
+TweenMax.from('.product p  .hide',1.5,{
+    delay:2.8,
+    y:'100%',
+    ease: Expo.easeInOut,
+})
+
+TweenMax.from('.product h2  .hide',1.5,{
+    delay:3,
+    y:'100%',
+    ease: Expo.easeInOut,
+})
+
+TweenMax.from('.product .btn a',1.5,{
+    delay:2.4,
+    y:'-70',
+    ease: Expo.easeInOut,
+})
+
+//product display animation 
+
+TweenMax.from('.doritos',1.5,{
+    delay:2.5,
+    y:'-100%',
+    ease: Expo.easeInOut,
+})
+
+TweenMax.from('.chips',1.5,{
+    delay:2.4,
+    x:'200%',
+    ease: Expo.easeInOut,
+})
+
+TweenMax.from('.ketchup',1.5,{
+    delay:2.8,
+    y:'200%',
+    ease: Expo.easeInOut,
+})
+
+TweenMax.from('.chilli',1.5,{
+    delay:2.5,
+    y:'200%',
+    ease: Expo.easeInOut,
+})
+TweenMax.from('.backdrop',1.5,{
+    delay:1.8,
+    y:'200%',
+    ease: Expo.easeInOut,
+})
+
+
